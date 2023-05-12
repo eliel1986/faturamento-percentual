14b7f1f5a318ae8384f90153630c761b107b7c5b
eliel1986 <eliel_1986@hotmail.com>
2023-05-11 21:26:06 -0300

    update

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 const form = document.querySelector("#form");
 
-const densOrig = document.querySelector("#dens-orig").value;
-const densDest = document.querySelector("#dens-dest").value;
-const pesoOrig = document.querySelector("#peso-orig").value;
-const pesoDest = document.querySelector("#peso-dest").value;
+const densOrig = document.querySelector("#dens-orig");
+const densDest = document.querySelector("#dens-dest");
+const pesoOrig = document.querySelector("#peso-orig");
+const pesoDest = document.querySelector("#peso-dest");
+const divteste = document.querySelector(".container");
 
-const result = pesoDest * densDest;
+const calcular = (number) => {
 
-const resume = (text) => {
-    const ele = document.createElement("div")
-    ele.classList.add("ele")
+    const geral = document.createElement("div");
+    geral.classList.add("resultado");
+    document.getElementById("aaa").style.display = "block";
 
-    
+    // const dif = document.createElement("input");
+    // dif.classList.add("abc");
+    // dif.value = number;
+    // geral.appendChild(dif);
+
+    // divteste.appendChild(geral);
+
+    const densidade = densOrig.value;
+
+    document.getElementById('dens_orig').value = densidade;
 }
+
 
 
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    if (result) {
-        resume(result)
+    const  result = densOrig.value;
+
+    if(result) {
+        calcular(result);
     }
 });
