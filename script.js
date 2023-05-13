c195f64d945e3c47ecb86f9dc3c1df12f8151eef
eliel1986 <eliel_1986@hotmail.com>
2023-05-12 22:42:09 -0300

    update

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const form = document.querySelector("#form");
 
-const densOrig = document.querySelector("#dens-orig");
-const densDest = document.querySelector("#dens-dest");
+const vol_vinte_orig = document.querySelector("#vol-vinte-origem");
+const vol_V_Uni = document.querySelector("#vol-vinte-uni");
+const dens_V_Uni = document.querySelector("#dens-vinte-uni");
 const pesoOrig = document.querySelector("#peso-orig");
-const pesoDest = document.querySelector("#peso-dest");
-const divteste = document.querySelector(".container");
+const pesoUni = document.querySelector("#peso-uni");
+const volDren = document.querySelector("#vol-dren");
+
+// const divteste = document.querySelector(".container");
 
 const calcular = (number) => {
 
-    const geral = document.createElement("div");
-    geral.classList.add("resultado");
+    // const geral = document.createElement("div");
+    // geral.classList.add("resposta");
     document.getElementById("aaa").style.display = "block";
+
 
     // const dif = document.createElement("input");
     // dif.classList.add("abc");
@@ -19,20 +23,51 @@ const calcular = (number) => {
 
     // divteste.appendChild(geral);
 
-    const densidade = densOrig.value;
+    const dif_vol_vinte = vol_vinte_orig.value - vol_V_Uni.value;
+    document.getElementById('dif_vol_vinte').value = `${dif_vol_vinte} litros`;
 
-    document.getElementById('dens_orig').value = densidade;
+    const dif_litros_vol = (dif_vol_vinte / vol_vinte_orig.value).toFixed(2)
+    document.getElementById('dif_vol_litros').value = `${dif_litros_vol} %`;
+
+    const dif_peso = pesoOrig.value - pesoUni.value
+    document.getElementById('dif_peso').value = `${dif_peso} Kilos`;
+
+    const dif_litros_peso = (dif_peso / (dens_V_Uni.value / 1000)).toFixed(0)
+    document.getElementById('dif_litros_peso').value = `${dif_litros_peso} litros`;
+
+    const dif_litros_dreno = Number(dif_litros_peso) + Number(volDren.value)
+    document.getElementById('desc_falta').value = `${dif_litros_dreno} litros`;
+
+    const perc_result = (((dif_vol_vinte - dif_litros_dreno) / vol_V_Uni.value)*100).toFixed(2)
+    document.getElementById('perc_falta').value = `${perc_result} %`;
+
+    if (perc_result <= 0.4) {
+        document.getElementById("aaa").style.backgroundColor = "green";
+    } else {
+        document.getElementById("aaa").style.backgroundColor = "red";
+    }
+
+    
+
+
 }
-
-
-
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const  result = densOrig.value;
+    const result = vol_vinte_orig.value;
 
     if(result) {
         calcular(result);
     }
 });
+
+// function reset() {
+
+//     vol_vinte_orig = '';
+//     vol_V_Uni = '';
+//     dens_V_Uni = '';
+//     pesoOrig = '';
+//     pesoUni = '';
+//     volDren = '';
+// }
