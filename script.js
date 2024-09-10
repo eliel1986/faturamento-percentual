cb22e822fbe0f6cf899990f647c55da15c5bc56f
Eliel Silva <79846053+eliel1986@users.noreply.github.com>
2024-09-09 21:56:03 -0300

    update

--- script.js
+++ script.js
@@ -8,84 +8,86 @@ const vol_vinte_ori = document.querySelector("#vol_vinte_origem");
 
 const calcular = () => {
 
-    document.getElementById("formulario_principal").style.display = "none";
-    document.getElementById("resposta").style.display = "block";
+	document.getElementById("formulario_principal").style.display = "none";
+	document.getElementById("resposta").style.display = "block";
 
-    const densOrig = document.getElementById('dens_orig').value;
+	const densOrig = document.getElementById('dens_orig').value;
 	const densDest = document.getElementById('dens_dest').value;
 	const pesoOrig = document.getElementById('peso_orig').value;
 	const pesoDest = document.getElementById('peso_dest').value;
-    const volDren = document.getElementById('vol_dren').value;
-    const volVinteOrig = document.getElementById('vol_vinte_origem').value;
+	const volDren = document.getElementById('vol_dren').value;
+	const volVinteOrig = document.getElementById('vol_vinte_origem').value;
 
-    document.getElementById('vol_vint_orig').value = `${volVinteOrig} litros`;
+	document.getElementById('vol_vint_orig').value = `${volVinteOrig} litros`;
 
-    	const pesoDest_densDest = (pesoDest / densDest).toFixed(0);		
-		document.getElementById('pDest_dDest').value = `${pesoDest_densDest} litros`;
+	const pesoDest_densDest = (pesoDest / densDest).toFixed(0);
+	document.getElementById('pDest_dDest').value = `${pesoDest_densDest} litros`;
 
-		const pesoDest_densOrig = (pesoDest / densOrig).toFixed(0);		
-		document.getElementById('pDest_dOrig').value = `${pesoDest_densOrig} litros`;
+	const pesoDest_densOrig = (pesoDest / densOrig).toFixed(0);
+	document.getElementById('pDest_dOrig').value = `${pesoDest_densOrig} litros`;
 
-		const dif_Dens = (pesoDest_densDest - pesoDest_densOrig).toFixed(0);
-		if (dif_Dens < 0) {
-			document.getElementById('dif_Dens').style.backgroundColor = 'red';
-		}
-		else {
-			document.getElementById('dif_Dens').style.backgroundColor = 'green';
-		}
-		document.getElementById('dif_Dens').value = `${dif_Dens} litros`;
-
-		const difPesos = (pesoDest - pesoOrig).toFixed(0);
-		document.getElementById('pDest_pOrig').value = `${difPesos} Kilos`;
-		if (difPesos < 0) {
-			document.getElementById('pDest_pOrig').style.backgroundColor = 'red';
-		}
-		else {
-			document.getElementById('pDest_pOrig').style.backgroundColor = 'green';
-		}
-
-		const difPesosdensDest = (difPesos / densDest).toFixed(0);
-		document.getElementById('dif_Peso').value = `${difPesosdensDest} litros`;
-		if (difPesosdensDest < 0) {
-			document.getElementById('dif_Peso').style.backgroundColor = 'red';
-		}
-		else {
-			document.getElementById('dif_Peso').style.backgroundColor = 'green';
-		}
-
-		const apurado = Number(dif_Dens) + Number(difPesosdensDest) + Number(volDren);
-		document.getElementById('result').value = `${apurado} litros`;
-		if (apurado <= 0) {
-			document.getElementById('result').style.backgroundColor = 'red';
-		}
-		else {
-			document.getElementById('result').style.backgroundColor = 'green';
-		}
-
-        const perc = ((apurado / volVinteOrig) * 100 ).toFixed(2);
-        document.getElementById('percent').value = `${perc} %`;
-
-        if (perc >= -0.4 && perc <= 0.4) {
-            document.getElementById("inform").style.backgroundColor = "green";
-            document.getElementById("inform").value = "LIBERADO";
-        } else {
-            document.getElementById("inform").style.backgroundColor = "red";
-            document.getElementById("inform").value = "CONTRA PROVA";
-        }
+	const dif_Dens = (pesoDest_densDest - pesoDest_densOrig).toFixed(0);
+	if (dif_Dens < 0) {
+		document.getElementById('dif_Dens').style.backgroundColor = 'red';
 	}
+	else {
+		document.getElementById('dif_Dens').style.backgroundColor = 'green';
+	}
+	document.getElementById('dif_Dens').value = `${dif_Dens} litros`;
+
+	const difPesos = (pesoDest - pesoOrig).toFixed(0);
+	document.getElementById('pDest_pOrig').value = `${difPesos} Kilos`;
+	if (difPesos < 0) {
+		document.getElementById('pDest_pOrig').style.backgroundColor = 'red';
+	}
+	else {
+		document.getElementById('pDest_pOrig').style.backgroundColor = 'green';
+	}
+
+	const difPesosdensDest = (difPesos / densDest).toFixed(0);
+	document.getElementById('dif_Peso').value = `${difPesosdensDest} litros`;
+	if (difPesosdensDest < 0) {
+		document.getElementById('dif_Peso').style.backgroundColor = 'red';
+	}
+	else {
+		document.getElementById('dif_Peso').style.backgroundColor = 'green';
+	}
+
+	const apurado = Number(dif_Dens) + Number(difPesosdensDest) + Number(volDren);
+	document.getElementById('result').value = `${apurado} litros`;
+	if (apurado <= 0) {
+		document.getElementById('result').style.backgroundColor = 'red';
+	}
+	else {
+		document.getElementById('result').style.backgroundColor = 'green';
+	}
+
+	const perc = ((apurado / volVinteOrig) * 100).toFixed(2);
+	document.getElementById('percent').value = `${perc} %`;
+
+	if (perc >= -0.4 && perc <= 0.4) {
+		document.getElementById("inform").style.backgroundColor = "green";
+		document.getElementById("inform").value = "LIBERADO";
+	} else {
+		document.getElementById("inform").style.backgroundColor = "red";
+		document.getElementById("inform").value = "CONTRA PROVA";
+	}
+}
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+	e.preventDefault();
 
-    const densOrig = dens_orig.value;
-    const densDest = dens_dest.value;
-    const pesoOrig = peso_orig.value;
-    const pesoDest = peso_dest.value;
+	const densOrig = dens_orig.value;
+	const densDest = dens_dest.value;
+	const pesoOrig = peso_orig.value;
+	const pesoDest = peso_dest.value;
 
-    if (pesoDest > pesoOrig) {
+	if (pesoDest > pesoOrig) {
 		alert('Peso de destino não pode ser maior que peso de origem')
+		document.querySelector("#peso_dest").focus()
+		return;
 	}
 	if (densOrig && densDest && pesoOrig && pesoDest) {
-        calcular();
-    }
+		calcular();
+	}
 });
